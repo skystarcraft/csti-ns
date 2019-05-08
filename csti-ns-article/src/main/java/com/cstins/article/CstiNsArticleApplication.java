aecb895baf4deb63ebab7d46e9275303e1c0c347
skystarcraft <18846432184@163.com>
2019-05-08 18:42:43 +0800

    更新build.gradle，增加打包带依赖

--- csti-ns-article/src/main/java/com/cstins/article/CstiNsArticleApplication.java
+++ csti-ns-article/src/main/java/com/cstins/article/CstiNsArticleApplication.java
@@ -3,7 +3,9 @@ package com.cstins.article;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,6 +14,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.cstins.article.dao")
 @EntityScan("com.cstins.article.entity")
 @ComponentScan(value = {"com.cstins.article.dao", "com.cstins.article.service", "com.cstins.article.api"})
+@EnableCircuitBreaker
+@EnableHystrix
 public class CstiNsArticleApplication {
 
     public static void main(String[] args) {
